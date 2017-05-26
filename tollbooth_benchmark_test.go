7ee995cf9658c5905cbd528ee7398c45088f927e
songrgg <songrgg0.0@gmail.com>
2017-05-26 23:29:08 +0800

    add benchmark

--- tollbooth_benchmark_test.go
+++ tollbooth_benchmark_test.go
@@ -35,3 +35,23 @@ func BenchmarkBuildKeys(b *testing.B) {
 		}
 	}
 }
+
+func BenchmarkBuildKeysWithLongKey(b *testing.B) {
+	limiter := NewLimiter(1, time.Second)
+	limiter.IPLookups = []string{"X-Real-IP", "X-Forwarded-For", "RemoteAddr"}
+	limiter.Headers = []string{"X-Auth-Token"}
+
+	request, err := http.NewRequest("GET", "/", strings.NewReader("Hello, world!"))
+	if err != nil {
+		fmt.Printf("Unable to create new HTTP request. Error: %v", err)
+	}
+
+	request.Header.Set("X-Auth-Token", "VI2YiplZDAAdit1gwgctj//avY52fFGsKVTC7nh8ewa2PMtw0eveUxiFtPHemTX8wG++z38Mwn5rXSwahn5gK1bEJOvQ8VUX2O2w4XA4ljA0xikiAJbcO75YCyHxuM7pV4F/Kz9TnritmuQSo8A0qB85Yq9MNamlXxuyNtRfP/neZwXjgrEFctDV6cSVCl71")
+	request.Header.Set("X-Forwarded-For", "193.22.33.3")
+	for i := 0; i < b.N; i++ {
+		sliceKeys := BuildKeys(limiter, request)
+		if len(sliceKeys) == 0 {
+			fmt.Print("Length of sliceKeys should never be empty.")
+		}
+	}
+}
